8c1a80f35a7235ce0531f8ac5907fb1d82a019fa
verothen <vjthen@ucdavis.edu>
2025-12-23 23:22:04 -0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+//Math.random() generates a random decimal [0,1)
+//Math.random()*20 scales the random decimal [0,19]
+//Math.floor rounds down decimal to an integer
+//+1 changes the range from [0,19] to [1,20]
 const myGuess = Math.floor(Math.random()*20)+1;
 let guesses = 0;
 let guess;
